bedeb7bda86b18e7114a799dabfa784680590801
Willian Birollo <willianbirollo@outlook.com>
2022-08-31 14:28:28 -0300

    First commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+        
+        public EventoController()
+        {            
+        }
+
+        public IEnumerable<Evento> _evento = new Evento[] 
+        {
+            new Evento(){
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "São Paulo",
+                Lote = "1º Lote",
+                QuantidadePessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "Foto.png"
+            },
+            new Evento(){
+                EventoId = 2,
+                Tema = "Angular e suas novidades",
+                Local = "Campinas",
+                Lote = "2º Lote",
+                QuantidadePessoas = 300,
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                ImagemURL = "Foto.png"
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{Id}")]
+        public IEnumerable<Evento> GetById(int Id)
+        {
+            return _evento.Where(evento => evento.EventoId == Id);
+        }
+
+    }
+}
